f8f174469880263a01ded39d16ad832efc5a2b9b
gencayunlu@gmail.com <gencayunlu@gmail.com>
2023-06-05 17:56:39 +0000

    calculate as model case1

--- models/staging/case_1.sql
+++ models/staging/case_1.sql
@@ -3,13 +3,13 @@
 
 
 
-select PRODUCT_ID, MONTH_, count(SATIS_ADEDI) from 
+select PRODUCT_ID, MONTH_, count(SATIS_ADEDI) as sum_satis from 
 (select  cast (substr(ORDER_DATE,6,2) as INT) as MONTH_,
 ORD.product_id as PRODUCT_ID, 
 (cast( total_amount as double) )/ (cast( PRODUCT_PRICE as double) ) as satis_adedi
 
-from RAW.ILAB.ORDERS ORD 
-JOIN RAW.ILAB.PRODUCTS PROD
+from {{ ref('raw_orders') }} ORD 
+JOIN {{ ref('raw_produtcs') }} PROD
 WHERE ORD.PRODUCT_ID = PROD.PRODUCT_ID
 ) as TT
 group by 1,2
